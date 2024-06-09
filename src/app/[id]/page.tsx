4c87b8fbc29b1ccb0b4531b3334c31bf8906ca8d
Improver2108 <yashrawat2108@gmail.com>
2024-06-09 10:50:51 +0530

    "Removed apostrophes in text, duplicated paragraph, and removed some sentences."

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -15,21 +15,22 @@ export default function ServicePage() {
       </h1>
       <div className="text-center px-5 text-[1.12rem] space-y-5 tracking-wide my-[1em] max-w-[80rem]">
         <p>
-          If you're looking for a premium bathroom remodel, USA Home Remodeling
+          If youre looking for a premium bathroom remodel, USA Home Remodeling
           would love to hear from you. We give our clients the beauty and
-          functionality they're looking for in a tranquil and refined bathroom
+          functionality theyre looking for in a tranquil and refined bathroom
           space. This includes updates to plumbing, reconceiving spatial
-          geometry, and installing that new tub or shower you've been waiting to
+          geometry, and installing that new tub or shower youve been waiting to
           enjoy! We also design your renovation with an eye for sustainability
           that allows you to reduce your carbon footprint
         </p>
         <p>
-          Most remodeling is meant to reflect the unique setting of the
-          resident's home and the overall design aesthetic of the larger
-          interior. Because of this, we examine lighting, faucets, shower,
-          heads, finishes, flooring, countertops, cabinetry, and every
-          consideration for making this room the beautiful retreat you want it
-          to be.
+          If youre looking for a premium bathroom remodel, USA Home Remodeling
+          would love to hear from you. We give our clients the beauty and
+          functionality theyre looking for in a tranquil and refined bathroom
+          space. This includes updates to plumbing, reconceiving spatial
+          geometry, and installing that new tub or shower youve been waiting to
+          enjoy! We also design your renovation with an eye for sustainability
+          that allows you to reduce your carbon footprint
         </p>
       </div>
       <div className="grid grid-cols-2 md:grid-cols-4 px-5 max-w-[80rem] gap-2 py-5">
